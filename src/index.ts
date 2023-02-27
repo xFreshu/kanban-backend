b086cc67c51050c51f7bb0c0596e98b2e4b66f34
xFreshu <lprzybyszcontact@gmail.com>
2023-02-27 17:20:17 +0100

    Add morgan as middlewear and dotenv package

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import express from 'express'
+import * as dotenv from 'dotenv'
+import morgan from 'morgan'
 import userRoute from './routes/userRoute'
 const app = express()
+dotenv.config()
 
-app.listen(3000, () => {
-    console.log('test')
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
 
+//Middlewares
+app.use(morgan('dev'))
 app.use('/api/user', userRoute)
