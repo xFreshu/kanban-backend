e167b4302af8ac013ae1117a2f43952b5e0e9879
xFreshu <lprzybyszcontact@gmail.com>
2023-03-01 15:26:31 +0100

    create basic create new user controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,9 +1,41 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
 export const getUsers = (req, res) => {
     res.send('user from controller')
 }
 
 export const getUserByID = (req, res) => {
     res.send('user :id from controller')
+}
+
+export const createUser = async (req, res, next) => {
+    const { email, password, companyName, board } = req.body
+    // validate existing user
+    const existingUser = await prisma.user.findUnique({
+        where: {
+            email: email,
+        },
+    })
+    if (existingUser) {
+        return res.status(400).json({
+            message: 'User already exists',
+        })
+    }
+    // create user
+    const user = await prisma.user.create({
+        data: {
+            email: email,
+            password: password,
+            companyName: companyName,
+            board: board,
+        },
+    })
+    res.status(201).json({
+        message: 'User created',
+        user: user,
+    })
 }
 
 export const updateUser = (req, res) => {
--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -4,11 +4,13 @@ import {
     getUserByID,
     deleteUser,
     updateUser,
+    createUser,
 } from '../controllers/userController'
 const router = Router()
 
 router.get('/', getUsers)
 router.get('/:id', getUserByID)
+router.post('/', createUser)
 router.patch('/:id', updateUser)
 router.delete('/:id', deleteUser)
 
